3b1c8697cbb6c1aad9fd1e35ec564962e28e3245
michaelwoelfel <woelfelm@t-online.de>
2024-01-13 15:51:28 +0700

    added chat + message

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 #Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        print("Received Data" + request.POST['textmessage'])
     return render(request, 'chat/index.html')
