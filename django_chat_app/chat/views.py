cf8c028f86f8717218456f3f247d77bbf34cc122
michaelwoelfel <woelfelm@t-online.de>
2024-01-16 15:05:36 +0700

    startet creating the login part

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from .models import Chat, Message
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 
 #Create your views here.
 
 def index(request):
     if request.method == 'POST':
         print("Received Data" + request.POST['textmessage'])
-    return render(request, 'chat/index.html')
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat,author=request.user,receiver=request.user)
+    chatMessages = Message.objects.filter(chat__id=1)   
+    return render(request, 'chat/index.html', {'messages':chatMessages})
+
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+        if user:
+            login(request, user)
+            return HttpResponseRedirect('/chat/')
+        else:
+            return render(request, 'login/login.html', {'wrongPassword': True})
+    return render(request, 'login/login.html')
+                  
--- django_chat_app/django_chat_app/urls.py
+++ django_chat_app/django_chat_app/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from chat.views import index
+from chat.views import index, login_view
 
 urlpatterns = [
     path('', index),  
     path('admin/', admin.site.urls),
     path('chat/', index),
+    path('login/',login_view),
 ]
